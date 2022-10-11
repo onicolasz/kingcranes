5ec22c4d320dfac2511c62b07a02eaadf99451ca
Nícolas Barros <98993369+onicolasz@users.noreply.github.com>
2022-10-10 22:41:59 -0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
